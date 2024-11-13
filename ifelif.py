43b4e02eb883977f09e89d0f8b0314fb1eac7cd9
Dione Braga <dionebraga.work@gmail.com>
2024-11-13 17:30:37 -0300

    Consegui

--- ifelif.py
+++ ifelif.py
@@ -1,8 +1,7 @@
 idade = 22
 if idade <18:
     print("Você é menor de idade.")
-elif idade >=18 and idade < 65 :
+elif idade >=18 and idade <65 :
     print("Voce é adulto.")
 else: 
     print("Você é idoso")
-    
